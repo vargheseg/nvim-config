b37b5b963865eaea1dc8651b34db0e7f60bda16a
geogin <geogin.varghese@mydomain.com>
2026-04-24 20:55:42 +0200

    blink-cmp cycle behavior change, tab to cycle enter to select

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,11 +4,26 @@ return {
     dependencies = { 'rafamadriz/friendly-snippets' },
     version = '1.*',
     opts = {
-      keymap = { preset = 'super-tab' },
+    keymap = {
+        preset = 'enter',           -- or 'none' if you want full control
+        ['<Tab>']   = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+
+        -- ['<Tab>']   = { 'snippet_forward', 'select_next', 'fallback' },
+        -- ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+      },
       appearance = {
         nerd_font_variant = 'mono'
       },
-      completion = { documentation = { auto_show = false } },
+      completion = {
+        documentation = { auto_show = false },
+        list = {
+          selection = {
+            preselect = false,     -- don't auto-select first item
+            auto_insert = false,   -- don't auto-insert on navigation
+          }
+        },
+      },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
